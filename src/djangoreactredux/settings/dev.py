86346c185a05332409c4b7540c39e647d89fef4e
andrej <navikom11@mail.ru>
2016-12-11 15:37:59 +0300

    init

---
+++ src/djangoreactredux/settings/dev.py
@@ -0,0 +1,69 @@
+import os
+
+from djangoreactredux.settings.base import *  # NOQA (ignore all errors on this line)
+
+
+DEBUG = True
+
+PAGE_CACHE_SECONDS = 1
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite'),  # NOQA (ignore all errors on this line)
+#     }
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'pano360_db',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'nadcukandrej',
+        'PASSWORD': '',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
+    }
+}
+
+REST_FRAMEWORK['EXCEPTION_HANDLER'] = 'django_rest_logger.handlers.rest_exception_handler'  # NOQA (ignore all errors on this line)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['django_rest_logger_handler'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'django_rest_logger_handler': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['django_rest_logger_handler'],
+            'propagate': False,
+        },
+        'django_rest_logger': {
+            'level': 'DEBUG',
+            'handlers': ['django_rest_logger_handler'],
+            'propagate': False,
+        },
+    },
+}
+
+DEFAULT_LOGGER = 'django_rest_logger'
+
+LOGGER_EXCEPTION = DEFAULT_LOGGER
+LOGGER_ERROR = DEFAULT_LOGGER
+LOGGER_WARNING = DEFAULT_LOGGER
